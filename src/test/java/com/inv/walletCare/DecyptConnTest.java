9360bb89df13f8a83ac1cea3a193723dc0b457be
ycc <yccopac@ucenfotec.ac.cr>
2024-07-14 09:54:56 -0600

    Allow decryption of application.properties when app is running. Include JUnit Test.

--- src/test/java/com/inv/walletCare/DecyptConnTest.java
+++ src/test/java/com/inv/walletCare/DecyptConnTest.java
@@ -28,10 +28,12 @@ public class DecyptConnTest {
     static void dynamicProperties(DynamicPropertyRegistry registry) {
         registry.add("jasypt.encryptor.password", () -> "my-secret-password");
         registry.add("spring.datasource.url", () -> "jdbc:mariadb://localhost:3306/walletcare");
+        registry.add("jasypt.encryptor.property.prefix", () -> "[[[[[");
+        registry.add("jasypt.encryptor.property.suffix", () -> "]]]]]");
     }
 
     @Test
-    void contextLoads() {
+    void datasourceConnPrint() {
         System.out.println(printDatabaseConn());
     }
 }
