047a7c10b3839f6299a955c0f0c9b014b786f47a
ycc <yccopac@ucenfotec.ac.cr>
2024-07-14 08:52:14 -0600

    Add encryption of application.properties username and password. Add environment variables for datasource url.

---
+++ src/test/java/com/inv/walletCare/DecyptConnTest.java
@@ -0,0 +1,37 @@
+package com.inv.walletCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.util.Map;
+
+@SpringBootTest
+public class DecyptConnTest {
+
+    @Value("${spring.datasource.password}")
+    private String mariaPassword;
+
+    @Value("${spring.datasource.url}")
+    private String mariaURL;
+
+    @Value("${spring.datasource.username}")
+    private String mariaUser;
+
+    public Map<String, String> printDatabaseConn() {
+        return Map.of("apiURL", mariaURL, "apiPassword", mariaPassword, "apiUsername", mariaUser);
+    }
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("jasypt.encryptor.password", () -> "my-secret-password");
+        registry.add("spring.datasource.url", () -> "jdbc:mariadb://localhost:3306/walletcare");
+    }
+
+    @Test
+    void contextLoads() {
+        System.out.println(printDatabaseConn());
+    }
+}
