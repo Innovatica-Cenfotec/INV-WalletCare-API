edf8b61cbf4535ceb85ecf048755968ee70dd831
Guillermo Parini <68527063+Parini19@users.noreply.github.com>
2024-07-13 18:28:40 -0600

    Final version encryptionService and tests
    Se crea el javadoc del servicio de encriptado y desencriptado.
    Se añade el test del servicio de encriptado

--- src/main/java/com/inv/walletCare/logic/entity/auth/encryption/EncryptionService.java
+++ src/main/java/com/inv/walletCare/logic/entity/auth/encryption/EncryptionService.java
@@ -8,6 +8,10 @@ import javax.crypto.SecretKey;
 import java.util.Base64;
 
 /**
+ * Service class designed to encrypt and decrypt data using AES algorithm.
+ * This class provides methods to securely encrypt and decrypt data, ensuring
+ * confidentiality of sensitive information.
+ *
  * @author Guillermo Parini
  */
 @Service
@@ -17,12 +21,25 @@ public class EncryptionService {
 
     private final SecretKey secretKey;
 
+    /**
+     * Constructor for EncryptionService.
+     * Initializes the AES key generator and generates a secret key with a length of 256 bits.
+     *
+     * @throws Exception if an error occurs during key generation
+     */
     public EncryptionService() throws Exception {
         KeyGenerator keygen = KeyGenerator.getInstance(ALGORITHM);
-        keygen.init(256); //Cantidad de bits segun lo necesitemos
+        keygen.init(256);
         this.secretKey = keygen.generateKey();
     }
 
+    /**
+     * Encrypts the given data using AES algorithm.
+     *
+     * @param data the data to be encrypted
+     * @return the encrypted data encoded in Base64 format
+     * @throws Exception if an error occurs during encryption
+     */
     public String encrypt(String data) throws Exception {
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
@@ -30,6 +47,13 @@ public class EncryptionService {
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
+    /**
+     * Decrypts the given encrypted data using AES algorithm.
+     *
+     * @param encryptedData the data to be decrypted, in Base64 encoded format
+     * @return the decrypted data as a plain string
+     * @throws Exception if an error occurs during decryption
+     */
     public String decrypt(String encryptedData) throws Exception {
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
@@ -38,3 +62,4 @@ public class EncryptionService {
         return new String(decrypted);
     }
 }
+
--- src/test/java/com/inv/walletCare/logic/entity/auth/encryption/EncryptionServiceTest.java
+++ src/test/java/com/inv/walletCare/logic/entity/auth/encryption/EncryptionServiceTest.java
@@ -1,5 +1,6 @@
 package com.inv.walletCare.logic.entity.auth.encryption;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,9 +16,48 @@ class EncryptionServiceTest {
     @Autowired
     EncryptionService encryptionService;
 
+    /**
+     * Correct encryption and decryption configuration
+     *
+     * @throws Exception
+     */
     @Test
-    @DisplayName("Correct Encryption")
-    void encrypt_corret(){
+    @DisplayName("Encrypt and decrypt data successfully")
+    void testEncryptAndDecrypt(){
+        try {
+            String originalData = "SensitiveData123";
+            String encryptedData = encryptionService.encrypt(originalData);
+            String decryptedData = encryptionService.decrypt(encryptedData);
 
+            Assertions.assertEquals(originalData, decryptedData, "Decrypted data should match the original data");
+        } catch (Exception e) {
+            Assertions.fail("Encrypt and decrypt data failed with exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Encrypt and decrypt empty data")
+    void testEncryptAndDecryptEmptyData(){
+        try {
+            String originalData = "";
+            String encryptedData = encryptionService.encrypt(originalData);
+            String decryptedData = encryptionService.decrypt(encryptedData);
+
+            Assertions.assertEquals(originalData, decryptedData,"Decrypted data should match the original data");
+        } catch (Exception e) {
+            Assertions.fail("Encryption or decryption empty data failed with exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Decrypt invalid data")
+    void testDecryptInvalidData(){
+        try {
+            String invalidData = "This is invalid data";
+            encryptionService.decrypt(invalidData);
+            Assertions.fail("Decryption should have failed for invalid data");
+        } catch (Exception e) {
+            Assertions.assertTrue(true, "Exception was thrown as expected for invalid data");
+        }
     }
 }
