889841eafacf786128e11f473ca6ba4382a2638c
Juan Carlos Moscoso A. <jmoscosoa@ucenfotec.ac.cr>
2024-07-18 13:16:48 -0600

    Se agregan las validaciones faltantes.

---
+++ src/main/java/com/inv/walletCare/logic/entity/Response.java
@@ -0,0 +1,28 @@
+package com.inv.walletCare.logic.entity;
+
+/**
+ * Represents a generic response structure for API operations.
+ * This class encapsulates a simple message, typically used for conveying information about the result of an operation.
+ */
+public class Response {
+    // The message to be conveyed in the response.
+    private String message;
+
+    /**
+     * Constructs a new Response object with a specified message.
+     *
+     * @param message The message that this response should carry.
+     */
+    public Response(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Retrieves the message contained in this response.
+     *
+     * @return The message of this response.
+     */
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/inv/walletCare/logic/entity/user/UserRepository.java
+++ src/main/java/com/inv/walletCare/logic/entity/user/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long>  {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByNickname(String username);
+
+    Optional<User> findByIdentificationNumber(String identificationNumber);
 }
