40b553ce09e0944ed811b702ff375fa842168901
Juan Carlos Moscoso A. <jmoscosoa@ucenfotec.ac.cr>
2024-07-26 18:45:10 -0600

    se agrega el registar ingresos

---
+++ src/main/java/com/inv/walletCare/logic/entity/FrequencyTypeEnum.java
@@ -0,0 +1,11 @@
+package com.inv.walletCare.logic.entity;
+
+/**
+ * Enumerates the possible types of frequency
+ */
+public enum FrequencyTypeEnum {
+    MONTHLY,
+    ANNUAL,
+    BIWEEKLY,
+    OTHER
+}
---
+++ src/main/java/com/inv/walletCare/logic/entity/IncomeExpenceType.java
@@ -0,0 +1,9 @@
+package com.inv.walletCare.logic.entity;
+
+/**
+ * Enum that represents the type of income or expense.
+ */
+public enum IncomeExpenceType {
+    UNIQUE,
+    RECURRENCE;
+}
