24d3e47596227b94283f37a023d17217a87d72b2
ycc <yccopac@ucenfotec.ac.cr>
2024-08-18 02:01:03 -0600

    Function getAnualAmountByCategory for expense and income with test data. Add BarchartDTO and BarchartItemDTO.

--- src/main/java/com/inv/walletCare/logic/entity/report/ReportService.java
+++ src/main/java/com/inv/walletCare/logic/entity/report/ReportService.java
@@ -2,21 +2,90 @@ package com.inv.walletCare.logic.entity.report;
 
 import com.inv.walletCare.logic.entity.expense.Expense;
 import com.inv.walletCare.logic.entity.expense.ExpenseRepository;
+import com.inv.walletCare.logic.entity.income.IncomeRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ReportService {
     private final ExpenseRepository expenseRepository;
+    private final IncomeRepository incomeRepository;
 
-    public ReportService(ExpenseRepository expenseRepository) {
+    public ReportService(ExpenseRepository expenseRepository, IncomeRepository incomeRepository) {
         this.expenseRepository = expenseRepository;
+        this.incomeRepository = incomeRepository;
     }
 
-    public List<ExpenseReportDTO> getYearlyExpenseReport(long userId) {
+    public List<BarchartDTO> getYearlyExpenseByCategoryReport(int year, long userId) {
         List<Expense> expenses = expenseRepository.findAllByUserId(userId);
-        return null;
+
+        List<BarchartDTO> yearlyReport = new ArrayList<>();
+        List<BarchartItemDTO> items = new ArrayList<>();
+
+        BarchartItemDTO item = new BarchartItemDTO();
+        item.setMonth("ene");
+        item.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item);
+
+        BarchartItemDTO item2 = new BarchartItemDTO();
+        item2.setMonth("dic");
+        item2.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item2);
+
+        BarchartItemDTO item3 = new BarchartItemDTO();
+        item3.setMonth("feb");
+        item3.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item3);
+
+        BarchartItemDTO item4 = new BarchartItemDTO();
+        item4.setMonth("abr");
+        item4.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item4);
+
+        BarchartDTO barchartObject = new BarchartDTO();
+        barchartObject.setCategory("comida");
+        barchartObject.setData(items);
+
+        yearlyReport.add(barchartObject);
+
+        return yearlyReport;
+    }
+
+    public List<BarchartDTO> getYearlyIncomeByCategoryReport(int year, long userId) {
+        List<Expense> expenses = expenseRepository.findAllByUserId(userId);
+
+        List<BarchartDTO> yearlyReport = new ArrayList<>();
+        List<BarchartItemDTO> items = new ArrayList<>();
+
+        BarchartItemDTO item = new BarchartItemDTO();
+        item.setMonth("ene");
+        item.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item);
+
+        BarchartItemDTO item2 = new BarchartItemDTO();
+        item2.setMonth("dic");
+        item2.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item2);
+
+        BarchartItemDTO item3 = new BarchartItemDTO();
+        item3.setMonth("feb");
+        item3.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item3);
+
+        BarchartItemDTO item4 = new BarchartItemDTO();
+        item4.setMonth("abr");
+        item4.setAmount(BigDecimal.valueOf(120000.00));
+        items.add(item4);
+
+        BarchartDTO barchartObject = new BarchartDTO();
+        barchartObject.setCategory("comida");
+        barchartObject.setData(items);
+
+        yearlyReport.add(barchartObject);
+
+        return yearlyReport;
     }
 }
