781ff22a3deb42d1874bce144e4b5db28d497159
Daniel Castro <100711317+DanCasV27@users.noreply.github.com>
2024-08-23 01:27:14 -0600

    cambios para que la contraseña no sea cambiada al OTP y evitar error de balance en saving
    cambios para que la contraseña no sea cambiada al OTP y evitar error de balance en saving

--- src/main/java/com/inv/walletCare/rest/saving/SavingRestController.java
+++ src/main/java/com/inv/walletCare/rest/saving/SavingRestController.java
@@ -129,10 +129,14 @@ public class SavingRestController {
         newSaving.setCreatedAt(new Date());
         newSaving.setUpdatedAt(new Date());
         newSaving.setDeleted(false);
+        newSaving.setBalance(BigDecimal.valueOf(0));
+
 
         if (saving.getType() == SavingTypeEnum.RECURRENCE) {
             newSaving.setFrequency(saving.getFrequency());
             newSaving.setScheduledDay(saving.getScheduledDay());
+            System.out.println(saving.getTargetDate());
+            newSaving.setTargetDate(saving.getTargetDate());
         }
 
         Saving savingCreated = savingRepository.save(newSaving);
